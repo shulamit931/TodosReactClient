ae879c6638cf4f81ea7f2ec0989190f11bae7b6e
shulamit <shlamit931@gmail.com>
2023-05-03 22:29:31 +0300

    everything

---
+++ build/precache-manifest.9006285b10253a05d848bc5f5164edfa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f09f7a425337b8a8ec021c2e11285c6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e28751507677240ea76",
+    "url": "/static/js/2.d85aee39.chunk.js"
+  },
+  {
+    "revision": "abf6fcace3adbf8ab345683d164de87d",
+    "url": "/static/js/2.d85aee39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f31a70b159d9563d3c0",
+    "url": "/static/js/main.b3c68c93.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.b3c68c93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed24628c41b89e511d36",
+    "url": "/static/js/runtime-main.76e1faf0.js"
+  }
+]);
